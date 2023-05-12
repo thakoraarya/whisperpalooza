42331a58ae82b3a9a8fd144a05817e4b9ae27b4b
thakoraarya <aarya191102@gmail.com>
2023-05-12 07:01:28 +0530

    auth0 from frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { useAuth0 } from '@auth0/auth0-react';
+
+
 
 function App() {
+
+  const { loginWithPopup, loginWithRedirect, logout, user, isAuthenticated } = useAuth0()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Whisper Palooza - We Can See Your Messages But Its Free To Chat</p>
+
+      <ul>
+        <li>
+          <button onClick={loginWithPopup}>login with popup</button>
+        </li>
+        <li>
+          <button onClick={loginWithRedirect}>login with redirect</button>
+        </li>
+        <li>
+          <button onClick={logout}>logout</button>
+        </li>
+      </ul>
+
+      <h4>
+        {isAuthenticated ? 'logged in' : 'please login'}
+      </h4>
+
+      {isAuthenticated && (<pre>{JSON.stringify(user, null, 2)}</pre>)}
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
+// import dotenv from 'react-dotenv'
+// const dotenv = require('dotenv');
+// dotenv.config();
+
+
+
+const DomainName = 'replace your Domain name'
+const ClientId = 'replace your client id'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider domain={DomainName} clientId={ClientId} authorizationParams={{ redirect_uri: window.location.origin, }} >
+
+      <App />
+
+    </Auth0Provider>
   </React.StrictMode>
 );
 
